1fca4fb459ea1356835868d0f89f40d48823f514
jshields123 <jackshields@live.co.uk>
2021-01-02 16:27:23 +0000

    fizzbuzz code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,13 @@
-# jshields123 did you get this 
+# jshields123 did you get this
 
 def fizzbuzz(number)
-  "fizz"
+  if number % 3 == 0 && number % 5 == 0
+     "fizzbuzz"
+  elsif number % 3 == 0
+     "fizz"
+  elsif number % 5 == 0
+     "buzz"
+  else
+     "#{number}"
+  end
 end
-
